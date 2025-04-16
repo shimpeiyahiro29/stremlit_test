6c2ba662a409baa2acbfc226184f6571be228826
shinpei_yahiro <shinpei.yahiro29@gmail.com>
2025-04-16 21:04:30 +0900

    no message

---
+++ openAItesut.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+from openai import OpenAI
+
+
+text = st.text_input('質問したいことを聞いてね') # テキスト入力
+
+
+
+YOUR_API_KEY = "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"  # ご自身の API キーに置き換えてください
+client = OpenAI(api_key=YOUR_API_KEY, base_url="https://api.perplexity.ai")
+
+messages = [
+        {
+            "role": "system",
+            "content": (
+                "You are an artificial intelligence assistant and you need to "
+                "engage in a helpful, detailed, polite conversation with a user."
+            ),
+        },
+        {
+            "role": "user",
+            "content": (
+                text
+            ),
+        },
+    ]
+
+response = client.chat.completions.create(
+            model="sonar-pro",
+            messages=messages,
+)
+
+response.choices[0].message.content# テキスト表示
+
